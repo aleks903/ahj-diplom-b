94a7b0c3c5448af9886b4dba5928a45cdf41d86f
Kelakhsaev Aleksandr <aleks903@rambler.ru>
2020-03-09 22:35:40 +0300

    transfer

--- server.js
+++ server.js
@@ -8,10 +8,13 @@ const fs = require('fs');
 const uuid = require('uuid');
 const WS = require('ws');
 
+const fetch = require('node-fetch');
+
 const app = new Koa();
 
 const public = path.join(__dirname, '/public')
 app.use(koaStatic(public));
+
 // CORS
 app.use(async (ctx, next) => {
   const origin = ctx.request.get('Origin');
@@ -45,6 +48,7 @@ app.use(async (ctx, next) => {
   }
 });
 
+
 app.use(koaBody({
   text: true,
   urlencoded: true,
@@ -58,15 +62,46 @@ const wsServer = new WS.Server({ server });
 
 const arrMessges = [];
 
-const clients = [];
+let initMsg = false;
 
-router.get('/index', async (ctx, next) => {
-  console.log('get index');
-  // ctx.response.body = 'hello';
+router.get('/initmsg', async (ctx, next) => {
+  if (!initMsg) {
+    initMsg = true;
+
+    const resp = await fetch('http://localhost:7070/msg.json');
+    const body = await resp.text();
+    // console.log(body);
+    const arrInitMsg = JSON.parse(body);
+    arrMessges.push(...arrInitMsg);
+  
+    ctx.response.body = arrMessges[0];
+  }
+  ctx.response.body = 'ok';
 });
 
-router.get('/msg', async (ctx, next) => {
-  console.log('get index');
+// router.get('/msg', async (ctx, next) => {
+//   console.log('get index');
+//   ctx.response.body = arrMessges;
+//   // ctx.response.body = 'hello';
+// });
+
+router.get('/msg/:numb', async (ctx, next) => {
+  // console.log('get index');
+  console.log('get numb', ctx.params.numb);
+  // console.log(arrMessges[ctx.params.numb]);
+  const endArr = arrMessges.length - ctx.params.numb;
+  const startArr = (endArr - 10) < 0 ? 0 : (endArr - 10);
+  const returnArr = arrMessges.slice(startArr, endArr).reverse();
+
+  // for (const item of returnArr) {
+  //   [...wsServer.clients]
+  //     .filter(o => {
+  //       return o.readyState === WS.OPEN;
+  //     })
+  //     .forEach(o => o.send(item));
+  // }
+
+  ctx.response.body = returnArr;
   // ctx.response.body = 'hello';
 });
 
@@ -75,35 +110,63 @@ router.get('/users', async (ctx, next) => {
   ctx.response.body = clients;
 });
 
-router.post('/users', async (ctx, next) => {
+router.post('/msg', async (ctx, next) => {
+
   // create new contact
-  clients.push({...ctx.request.body, id: uuid.v4()});
+  const msgOb = {...ctx.request.body};
+  arrMessges.push(msgOb);
+  // [...wsServer.clients][0].send(JSON.stringify(msgOb));
+    [...wsServer.clients]
+    .filter(o => {
+      return o.readyState === WS.OPEN;
+    })
+    .forEach(o => o.send(JSON.stringify(msgOb)));
+    // console.log({...ctx.request.body});
   ctx.response.status = 204
 });
 
-router.delete('/users/:name', async (ctx, next) => {
-  // remove contact by id (ctx.params.id)
-  console.log(ctx.params.name);
-  const index = clients.findIndex(({ name }) => name === ctx.params.name);
-  if (index !== -1) {
-    clients.splice(index, 1);
-  };
-  ctx.response.status = 204
-});
+// router.post('/initmsg', async (ctx, next) => {
+
+//   // create new contact
+//   const msgOb = {...ctx.request.body};
+//   arrMessges.push(msgOb);
+//   // [...wsServer.clients][0].send(JSON.stringify(msgOb));
+//     [...wsServer.clients]
+//     .filter(o => {
+//       return o.readyState === WS.OPEN;
+//     })
+//     .forEach(o => o.send(JSON.stringify(msgOb)));
+//     // console.log({...ctx.request.body});
+//   ctx.response.status = 204
+// });
+
+// router.delete('/users/:name', async (ctx, next) => {
+//   // remove contact by id (ctx.params.id)
+//   console.log(ctx.params.name);
+//   const index = clients.findIndex(({ name }) => name === ctx.params.name);
+//   if (index !== -1) {
+//     clients.splice(index, 1);
+//   };
+//   ctx.response.status = 204
+// });
 
 wsServer.on('connection', (ws, req) => {
   console.log('connection');
-  ws.on('message', msg => {
-    console.log('msg');
+  ws.on('message', (msg) => {
+    // console.log(msg);
+    // add in arr message
+    arrMessges.push(msg);
+
     [...wsServer.clients]
     .filter(o => {
       return o.readyState === WS.OPEN;
     })
     .forEach(o => o.send(msg));
   });
-  ws.on('close', msg => {
+  ws.on('close', (msg) => {
     console.log('close');
     [...wsServer.clients]
+
     .filter(o => {
       return o.readyState === WS.OPEN;
     })
